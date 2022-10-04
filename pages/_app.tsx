eb1e9c2f94e200370db2bb1ac435cc9503698980
GeneralDeD <sayfiddinovsh.2000@gmail.com>
2022-10-04 12:19:47 +0500

    added nprogress package to app, for showing pages loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,21 @@ import { store, wrapper } from "../store/store";
 import Sidebar from "../components/sidebar";
 import Head from "next/head";
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router from "next/router";
+
+NProgress.configure({
+	minimum: 0.3,
+	easing: "ease",
+	speed: 800,
+	showSpinner: false,
+});
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
